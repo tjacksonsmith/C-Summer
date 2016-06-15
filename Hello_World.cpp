f6ad061d3b534dc74a0e35130a6920b125f8c349
tjacksonsmith <smitim12@evergreen.edu>
2016-06-15 13:27:43 -0700

    Add files via upload

---
+++ Hello_World.cpp
@@ -0,0 +1,30 @@
+//
+//  main.cpp
+//  Hello_World
+//
+//  Created by Timothy Smith on 6/15/16.
+//  Copyright © 2016 Timothy Smith. All rights reserved.
+//
+
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include <forward_list>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+#include <array>
+#include <regex>
+#include<random>
+#include<stdexcept>
+
+int main()
+{
+    std::cout<<" Hello World\n";
+    return 0;
+}
