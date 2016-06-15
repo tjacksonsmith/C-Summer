0a1607b35eb58a7327e3c59cc8e57fd8b1ae4437
tjacksonsmith <smitim12@evergreen.edu>
2016-06-15 16:03:24 -0700

    Add files via upload

---
+++ Name_Age.cpp
@@ -0,0 +1,37 @@
+// Timothy Smith
+//  Section 3.3 Programming and Principles
+
+
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include <forward_list>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+#include <array>
+#include <regex>
+#include<random>
+#include<stdexcept>
+using namespace std;
+
+
+int main()
+{
+    cout<< " Please enter your first name and age\n";
+    string first_name;
+    int age;
+    cin>>first_name;
+    cin>>age;
+    cout<<"Hello,"<<first_name<<"(age "<<age<<")\n";
+    
+    
+
+    
+    
+}
