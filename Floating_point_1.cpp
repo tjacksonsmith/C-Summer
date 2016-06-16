eca58b782f033a6cbe98465d84f5e211163dea2d
tjacksonsmith <smitim12@evergreen.edu>
2016-06-16 11:24:21 -0700

    Add files via upload

---
+++ Floating_point_1.cpp
@@ -0,0 +1,45 @@
+//
+//  main.cpp
+//  Floating point numbers_1
+//
+//  Created by Timothy Smith on 6/16/16.
+//  Copyright © 2016 Timothy Smith. All rights reserved.
+//
+
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include <forward_list>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+#include <array>
+#include <regex>
+#include<random>
+#include<stdexcept>
+using namespace std;
+
+
+int main()
+{
+    cout<< " Please enter floating point";
+    double n;
+    cin>> n;
+    cout<< "n=="<< n
+        <<"\nn+1=="<<n+1
+        <<"\n three times n=="<<3*n
+        <<"\n twice n=="<< n+n
+        <<"\n squared ==" << n*n
+        <<"\n half of n==" << n/2
+        <<"\n square root of n=="<< sqrt(n)
+        << '\n'; // name fore newline(end of line) in output
+
+    
+    
+    
+}
