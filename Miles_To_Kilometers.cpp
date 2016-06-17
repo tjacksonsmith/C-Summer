0bb3619bb7d7f646c9bfac903b381ff36581b8ae
tjacksonsmith <smitim12@evergreen.edu>
2016-06-17 15:47:22 -0700

    Add files via upload

---
+++ Miles_To_Kilometers.cpp
@@ -0,0 +1,37 @@
+//
+//  Miles to Kilometers.cpp
+//  Problem_3.2
+//
+//  Created by Timothy Smith on 6/17/16.
+//  Copyright © 2016 Timothy Smith. All rights reserved.
+//
+
+#include<iostream>
+#include<iomanip>
+#include<fstream>
+#include<sstream>
+#include<cmath>
+#include<cstdlib>
+#include<string>
+#include<list>
+#include <forward_list>
+#include<vector>
+#include<unordered_map>
+#include<algorithm>
+#include <array>
+#include <regex>
+#include<random>
+#include<stdexcept>
+using namespace std;
+
+
+int main()
+{
+    double miles;
+    cout<< " Please enter amount of miles";
+    cin>>miles;
+    cout<<"That's also "<<miles*1.609<< " kilometers!\n";
+    return 0;
+    
+}
+
