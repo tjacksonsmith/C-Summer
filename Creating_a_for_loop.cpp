fe7eeeae215bcb033bd2042027b52803bbdbb597
tjacksonsmith <smitim12@evergreen.edu>
2016-06-20 14:09:06 -0700

    Update Creating_a_for_loop.cpp

--- Creating_a_for_loop.cpp
+++ Creating_a_for_loop.cpp
@@ -1,40 +1,5 @@
-
-//
-//  main.cpp
-//  Test
-//
-//  Created by Timothy Smith on 6/20/16.
-//  Copyright © 2016 Timothy Smith. All rights reserved.
-//
-
-
-#include <iostream>
-#include<iostream>
-#include<iomanip>
-#include<fstream>
-#include<sstream>
-#include<cmath>
-#include<cstdlib>
-#include<string>
-#include<list>
-#include <forward_list>
-#include<vector>
-#include<unordered_map>
-#include<algorithm>
-#include <array>
-#include <regex>
-#include<random>
-#include<stdexcept>
-using namespace std;
-
-
-//
-//  main.cpp
-//  Test
-//
-//  Created by Timothy Smith on 6/20/16.
-//  Copyright © 2016 Timothy Smith. All rights reserved.
-//
+// Timothy Smith
+// Creating_a _for_loop
 
 
 #include <iostream>
