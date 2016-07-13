2fee44d2fe4d60a800701dac11834c362ee6e592
tjacksonsmith <smitim12@evergreen.edu>
2016-07-13 15:04:12 -0700

    Update Drill_Chapter6.cpp

--- Drill_Chapter6.cpp
+++ Drill_Chapter6.cpp
@@ -166,7 +166,7 @@ double primary()
                                                t = ts.get();
                                                break;
                                            case '-':
-                                               left += term();    // evaluate Term and subtract
+                                               left -= term();    // evaluate Term and subtract
                                                t = ts.get();
                                                break;
                                            default: 
