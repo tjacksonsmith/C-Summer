6357ba2e5e1084d597fbc593c4d8361132a0baad
tjacksonsmith <smitim12@evergreen.edu>
2016-07-13 15:05:06 -0700

    Update Chapter6 Problem 2.cpp

--- Chapter6 Problem 2.cpp
+++ Chapter6 Problem 2.cpp
@@ -165,7 +165,7 @@ double expression()
                 t = ts.get();
                 break;
             case '-':
-                left += term();    // evaluate Term and subtract
+                left -= term();    // evaluate Term and subtract
                 t = ts.get();
                 break;
             default:
